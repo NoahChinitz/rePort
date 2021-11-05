655aa89ac6d70234cc102d25719108ff7b142195
NoahChinitzGWU <noahchinitz@gwmail.gwu.edu>
2021-11-05 13:43:37 -0400

    Need to implement Ctrl+C exit

---
+++ rePort.py
@@ -0,0 +1,63 @@
+import socket
+import time
+import signal
+
+# Class that passes an exception if we timeout
+class TimeoutException(Exception):
+    pass
+
+# Actual timeout that raises the exception
+def timeout_handler(signum, frame):
+    raise TimeoutException
+
+# Port scanning function that handles connection to specified IP
+def rePort(port):
+    # Create a socket connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # We need a signal here if we timeout
+    signal.signal(signal.SIGALRM, timeout_handler)
+    try:
+        # Sound the alarm if we take longer than one second
+        signal.alarm(1)
+        # Connect to the target IP and Port
+        sock.connect((target_ip, port))
+        # If seconds is zero, any pending alarm is canceled
+        signal.alarm(0)
+        # Return True if we have a successful connection
+        return True
+    except:
+        # If we take longer than one second then we need to return False
+        return False
+
+# User Input
+ip = input('Who do you want to scan?: ')
+start_port = input('Starting Port?: ')
+end_port = input('Ending Port?: ')
+
+# Allows user to put in URL or IP
+target_ip = socket.gethostbyname(ip)
+
+print('Starting scan on host:', target_ip)
+
+# Start the timer
+start = time.time()
+
+# List to return all open ports
+open_ports = []
+
+# Go through the range of ports from start_port to end_port
+for port in range(int(start_port), int(end_port)+1):
+    if rePort(port):
+        open_ports.append(port)
+        print('Port ' + str(port) + ' is open')
+    else:
+        print('Port ' + str(port) + ' is closed')
+
+# End the timer
+end = time.time()
+
+# Print out open ports
+print('Open Ports: ', open_ports)
+
+# Print out time taken
+print(f'Time Taken: {end-start:.4f} seconds')
